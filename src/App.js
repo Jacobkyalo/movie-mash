ad07f454fd4fbfff6fbdc7e9e020559fe8c393c4
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-02-23 09:03:56 +0300

    created routes using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.scss";
 import Home from "./pages/home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/login/login";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "login",
+        element: <Login />,
+    },
+]);
 
 function App() {
     return (
         <div className="containerr">
-            <Home />
+            <RouterProvider router={router} />
         </div>
     );
 }
